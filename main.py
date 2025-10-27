eec4cca43ace988136b3150055ac52c70c98e7df
camurim <caamorimcoelho@gmail.com>
2025-10-27 16:33:56 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+from faker import Faker
+from rich.console import Console
+import argparse
+
+def parseargs():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-f",
+        "--field",
+        help="Lista de campos a serem populados (separados por vírgula).",
+        type=ascii,
+        required=True,
+    )
+
+    parser.add_argument(
+        "-t",
+        "--table",
+        help="Tabela a ser populada.",
+        type=ascii,
+        required=True,
+    )
+
+    parser.add_argument(
+        "-n",
+        "--number-of-registers",
+        help="Número de Registros a serem gerados.",
+        type=int,
+        required=False,
+        default=1
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    console = Console()
+    args = parseargs()
+
+    table:str = ""
+    field:str = ""
+    number:int = 1
+
+    if args.table:
+        table = str(args.table)
+
+    if args.field:
+        field = str(args.field)
+
+    if args.number_of_registers:
+        number = int(args.number_of_registers)
+
+    ar_fields = field.split(",")
+    
+    for i in range(number):
+        for f in ar_fields:
+            match f.lower():
+                case "nome":
+                    pass
+                case "cpf":
+                    pass
+                case "rg":
+                    pass
+                case "endereco" | "endereço":
+                    pass
+                case "telefone" | "fone" | "celular":
+                    pass
+        pass
+
+if __name__ == "__main__":
+    main()
